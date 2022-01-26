b6d1345cf8948b9fcf9070d92c246c37bff90449
takar <takartin@gmail.com>
2022-01-27 01:14:07 +0900

    서비스 부분 작성

---
+++ ex02/src/main/java/org/zerock/service/BoardServiceImpl.java
@@ -0,0 +1,21 @@
+package org.zerock.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.zerock.domain.BoardVO;
+import org.zerock.mapper.BoardMapper;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j;
+
+@Log4j
+@Service
+@AllArgsConstructor
+public abstract class BoardServiceImpl implements BoardService{
+
+		private BoardMapper mapper;
+		
+		@Override
+		public void register(BoardVO board) { }
+}
