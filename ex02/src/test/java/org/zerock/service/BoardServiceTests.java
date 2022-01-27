04b1547f95366f7d1eac1d5c56f3c7a9f4ead1a7
takar <takartin@gmail.com>
2022-01-28 02:33:49 +0900

    비즈니스 계층 부분 완료

--- ex02/src/test/java/org/zerock/service/BoardServiceTests.java
+++ ex02/src/test/java/org/zerock/service/BoardServiceTests.java
@@ -5,9 +5,10 @@ import static org.junit.Assert.assertNotNull;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.zerock.persistence.DataSourceTests;
+import org.zerock.domain.BoardVO;
 
 import lombok.Setter;
 import lombok.extern.log4j.Log4j;
@@ -26,4 +27,51 @@ public class BoardServiceTests {
 		log.info(service);
 		assertNotNull(service);
 	}
+	
+	@Test
+	public void testRegister() {
+		
+		BoardVO board = new BoardVO();
+		board.setTitle("새로 작성하는 글");
+		board.setContent("새로 작성하는 내용");
+		board.setWriter("newbie");
+	
+		service.register(board);
+		
+		log.info("생성된 게시물의 번호: " + board.getBno());
+	}
+	
+	@Test
+	public void testGetList() {
+		
+		service.getList().forEach(board -> log.info(board));
+	}
+	
+	@Test
+	public void testGet() {
+		
+		log.info(service.get(1L));
+	}
+	
+	@Test
+	public void testDelete() {
+		
+		log.info("REMOVE RESULT: "+service.remove(2L));
+		
+	}
+	
+	@Test
+	public void testUpdate() {
+		
+		BoardVO board = service.get(1L);
+		
+		if (board == null) {
+			return;
+		}
+		
+		board.setTitle("제목 수정합니다.");
+		log.info("MODIFY RESULT: "+ service.modify(board));
+	}
+	
+	
 }
