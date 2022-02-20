28dc193aadada09464643933cedd65247bdc8903
takar <takartin@gmail.com>
2022-02-21 04:05:22 +0900

    2/21 - Ajax와 Rest를 이용한 댓글처리

--- ex03/ex03/src/test/java/org/zerock/controller/ReplyMapperTests.java
+++ ex03/ex03/src/test/java/org/zerock/controller/ReplyMapperTests.java
@@ -89,4 +89,14 @@ public class ReplyMapperTests {
 		
 		replies.forEach(reply -> log.info(reply));
 	}
+	
+	@Test
+	public void testList2() {
+		
+		Criteria cri = new Criteria(2,10);
+		
+		List<ReplyVO> replies = mapper.getListWithPaging(cri, 212L);
+		
+		replies.forEach(reply -> log.info(reply));
+	}
 }
