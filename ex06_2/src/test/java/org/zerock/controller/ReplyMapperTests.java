c951e23fbe115771b8faae6c6f660c04d1726ceb
takar <takartin@gmail.com>
2022-06-05 03:19:33 +0900

    로그인 및 프로젝트 통합

---
+++ ex06_2/src/test/java/org/zerock/controller/ReplyMapperTests.java
@@ -0,0 +1,102 @@
+package org.zerock.controller;
+
+import java.util.List;
+import java.util.stream.IntStream;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.zerock.domain.Criteria;
+import org.zerock.domain.ReplyVO;
+import org.zerock.mapper.ReplyMapper;
+
+import lombok.Setter;
+import lombok.extern.log4j.Log4j;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({
+	"file:src/main/webapp/WEB-INF/spring/root-context.xml"
+})
+@Log4j
+public class ReplyMapperTests {
+
+	private Long[] bnoArr = { 212L, 211L, 210L, 209L, 208L};  
+	
+	@Setter(onMethod_ = @Autowired)
+	private ReplyMapper mapper;
+	
+	@Test
+	public void testCreate() {
+		
+		IntStream.rangeClosed(1, 10).forEach(i -> {
+			
+			ReplyVO vo = new ReplyVO();
+			
+			vo.setBno(bnoArr[i% 5]);
+			vo.setReply("´ñ±Û Å×½ºÆ®"+ i);
+			vo.setReplyer("replyer" + i);
+			
+			mapper.insert(vo);
+		});
+	}
+	
+	@Test
+	public void testMapper() {
+		log.info(mapper);
+	}
+	
+	@Test
+	public void testRead() {
+		
+		Long targetRno = 21L;
+		
+		ReplyVO vo = mapper.read(targetRno);
+		
+		log.info(vo);
+	}
+	
+	@Test
+	public void testDelete() {
+		
+		Long targetRno = 1L;
+		
+		mapper.delete(targetRno);
+	}
+	
+	@Test
+	public void testUpdate() {
+		
+		Long targetRno = 23L;
+		
+		ReplyVO vo = mapper.read(targetRno);
+		
+		vo.setReply("Update Reply ");
+		
+		int count = mapper.update(vo);
+		
+		log.info("UPDATE COUNT: " + count);
+	}
+	
+	@Test
+	public void testList() {
+		
+		Criteria cri = new Criteria();
+		
+		List<ReplyVO> replies = mapper.getListWithPaging(cri, bnoArr[0]);
+		
+		replies.forEach(reply -> log.info(reply));
+	}
+	
+	@Test
+	public void testList2() {
+		
+		Criteria cri = new Criteria(2,10);
+		
+		List<ReplyVO> replies = mapper.getListWithPaging(cri, 212L);
+		
+		replies.forEach(reply -> log.info(reply));
+	}
+}
